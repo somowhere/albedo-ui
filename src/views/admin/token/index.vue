a6f4c005fdf6a8c58159361526c352f9f6bc4620
Li Jie <somewhere0813@gmail.com>
2019-07-19 17:59:56 +0800

    update code

--- src/views/admin/token/index.vue
+++ src/views/admin/token/index.vue
@@ -1,115 +1,173 @@
-<!--
-  -    Copyright (c) 2018-2025, lengleng All rights reserved.
-  -
-  - Redistribution and use in source and binary forms, with or without
-  - modification, are permitted provided that the following conditions are met:
-  -
-  - Redistributions of source code must retain the above copyright notice,
-  - this list of conditions and the following disclaimer.
-  - Redistributions in binary form must reproduce the above copyright
-  - notice, this list of conditions and the following disclaimer in the
-  - documentation and/or other materials provided with the distribution.
-  - Neither the name of the pig4cloud.com developer nor the names of its
-  - contributors may be used to endorse or promote products derived from
-  - this software without specific prior written permission.
-  - Author: lengleng (wangiegie@gmail.com)
-  -->
+
 
 <template>
-  <div class="execution">
+  <div class="app-container calendar-list-container">
     <basic-container>
-      <avue-crud ref="crud"
-                 :page="page"
-                 :data="tableData"
-                 :table-loading="tableLoading"
-                 :option="tableOption"
-                 @on-load="getList"
-                 @refresh-change="refreshChange"
-                 @row-del="rowDel">
-        <template slot-scope="scope"
-                  slot="menu">
-          <el-button type="text"
-                     v-if="permissions.sys_token_del"
-                     icon="el-icon-delete"
-                     size="mini"
-                     plain
-                     @click="handleDel(scope.row,scope.index)">踢人
-          </el-button>
-        </template>
-      </avue-crud>
+      <el-row>
+
+        <el-col>
+          <div class="filter-container" v-show="searchFilterVisible">
+            <el-form :inline="true" ref="searchForm">
+              <el-form-item label="名称">
+                <el-input class="filter-item input-normal" size="small" v-model="listQuery.username"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button size="small" type="primary" icon="el-icon-search" @click="handleFilter">查询</el-button>
+                <el-button size="small" @click="searchReset" icon="el-icon-delete" >清空</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
+          <!-- 表格功能列 -->
+
+          <div class="table-menu">
+            <div class="table-menu-left">
+            </div>
+            <div class="table-menu-right">
+              <el-button icon="el-icon-search" circle size="small" @click="searchFilterVisible= !searchFilterVisible"></el-button>
+            </div>
+          </div>
+          <el-table  shadow="hover" :key='tableKey' @sort-change="sortChange" :data="list" v-loading="listLoading" element-loading-text="加载中..." fit highlight-current-row>
+            <el-table-column
+              type="index" fixed="left" width="50">
+            </el-table-column>
+            <el-table-column align="center" label="用户ID" width="200">
+              <template slot-scope="scope">
+                <span>{{scope.row.user_id}}</span>
+              </template>
+            </el-table-column>
+            <el-table-column align="center" label="用户名" width="150">
+              <template slot-scope="scope">
+                <span>{{scope.row.username}}</span>
+              </template>
+            </el-table-column>
+
+            <el-table-column align="center" label="令牌" width="250">
+              <template slot-scope="scope">
+              <span>
+                {{scope.row.access_token}}
+              </span>
+              </template>
+            </el-table-column>
+
+            <el-table-column align="center" label="类型" width="120">
+              <template slot-scope="scope">
+          <span>
+            {{scope.row.token_type}}
+          </span>
+              </template>
+            </el-table-column>
+
+            <el-table-column align="center" label="过期时间">
+              <template slot-scope="scope">
+                <el-tag>{{scope.row.expires_in}}</el-tag>
+              </template>
+            </el-table-column>
+
+            <el-table-column align="center" label="操作" fixed="right" width="60" v-if="sys_token_delete">
+              <template slot-scope="scope">
+                <el-button v-if="sys_token_delete" icon="icon-delete" title="删除" type="text" @click="handleDelete(scope.row)">
+                </el-button>
+              </template>
+            </el-table-column>
+
+          </el-table>
+          <div v-show="!listLoading" class="pagination-container">
+            <el-pagination class="pull-right" background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="listQuery.current" :page-sizes="[10,20,30, 50]" :page-size="listQuery.size" layout="total, sizes, prev, pager, next, jumper" :total="total">
+            </el-pagination>
+          </div>
+        </el-col>
+      </el-row>
     </basic-container>
   </div>
 </template>
 
 <script>
-  import {delObj, fetchList} from '@/api/admin/token'
-  import {tableOption} from '@/const/crud/admin/token'
-  import {mapGetters} from 'vuex'
-
+  import { removeToken, pageToken} from "./service";
+  import {mapGetters } from 'vuex';
+  import {parseJsonItemForm} from "@/util/util";
   export default {
-    name: 'token',
+    name: 'Token',
     data() {
       return {
-        tableData: [],
-        page: {
-          total: 0, // 总页数
-          currentPage: 1, // 当前页数
-          pageSize: 20 // 每页显示多少条
+        treeMenuData:[],
+        dialogFormVisible: false,
+        searchFilterVisible: true,
+        checkedKeys: [],
+        list: null,
+        total: null,
+        listLoading: true,
+        listQuery: {
+          current: 1,
+          size: 20
         },
-        tableLoading: false,
-        tableOption: tableOption
+        formEdit: true,
+        flagOptions: [],
+        dataScopeOptions:[],
+        sys_token_delete: false,
+        currentNode: {},
+        tableKey: 0
       }
     },
-    created() {
+    watch: {
     },
-    mounted: function () {
+    created() {
+      this.getList()
+      this.sys_token_delete = this.permissions["sys_token_del"];
     },
     computed: {
-      ...mapGetters(['permissions'])
+      ...mapGetters([
+        "permissions","dicts"
+      ])
     },
     methods: {
-      getList(page, params) {
-        this.tableLoading = true
-        fetchList(Object.assign({
-          current: page.currentPage,
-          size: page.pageSize
-        }, params)).then(response => {
-          this.tableData = response.data.records
-          this.page.total = response.data.total
-          this.tableLoading = false
-        })
+      getList() {
+        this.listLoading = true;
+        this.listQuery.params = {"username":this.listQuery.name}
+        pageToken(this.listQuery).then(response => {
+          this.list = response.data.records;
+          this.total = response.data.total;
+          this.listLoading = false;
+        });
       },
-      handleDel(row, index) {
-        this.$refs.crud.rowDel(row, index)
+      sortChange(column){
+        if(column.order=="ascending"){
+          this.listQuery.asc=column.prop
+          this.listQuery.desc=undefined;
+        }else{
+          this.listQuery.desc=column.prop
+          this.listQuery.asc=undefined;
+        }
+        this.getList()
       },
-      rowDel: function (row, index) {
-        var _this = this
-        this.$confirm('是否强制' + row.username+ '下线?', '提示', {
+
+      //搜索清空
+      searchReset() {
+        this.$refs['searchForm'].resetFields();
+      },
+      handleFilter() {
+        this.listQuery.current = 1;
+        this.getList();
+      },
+      handleSizeChange(val) {
+        this.listQuery.size = val;
+        this.getList();
+      },
+      handleCurrentChange(val) {
+        this.listQuery.current = val;
+        this.getList();
+      },
+      handleDelete(row) {
+        this.$confirm('此操作将永久删除, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
-        }).then(function () {
-          return delObj(row.access_token)
-        }).then(data => {
-          _this.tableData.splice(index, 1)
-          _this.$message({
-            showClose: true,
-            message: '删除成功',
-            type: 'success'
+        }).then(() => {
+          removeToken(row.id).then((rs) => {
+            this.getList();
           })
-        }).catch(function (err) {
         })
-      },
-      /**
-       * 刷新回调
-       */
-      refreshChange() {
-        this.getList(this.page)
       }
     }
   }
 </script>
-
-<style lang="scss" scoped>
-</style>
 
