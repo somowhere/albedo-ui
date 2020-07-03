8e946595cb68f3b50105fb689e07e6213fe1fe17
somewhere <837158334@qq.com>
2020-07-03 21:40:38 +0800

    update

---
+++ src/components/form/crud-date.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-date-picker
+    v-model="text"
+    :disabled="disabled"
+    :format="format"
+    :placeholder="placeholder?placeholder:`请输入${label}`"
+    :size="size"
+    :type="type"
+    :value-format="valueFormat"
+    @change="handleChange"
+  />
+</template>
+
+<script>
+export default {
+  name: 'CrudDate',
+  props: {
+    value: {
+      default: ''
+    },
+    type: {
+      default: 'date'
+    },
+    valueFormat: {
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    format: {
+      default: ''
+    },
+    size: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      text: ''
+    }
+  },
+  watch: {
+    value: function(n, o) {
+      this.text = this.value
+    }
+  },
+  created() {
+    this.text = this.value
+  },
+  mounted() {
+  },
+  methods: {
+    handleChange(value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/form/crud-select.vue
@@ -0,0 +1,84 @@
+<template>
+  <el-select
+    v-model="text"
+    style="width: 100%"
+    :clearable="clearable"
+    :disabled="disabled"
+    :filterable="filterable"
+    :multiple="multiple"
+    :placeholder="placeholder?placeholder:`请选择${label}`"
+    :size="size"
+    @change="handleChange"
+  >
+    <el-option v-for="(item,index) in dic" :key="index" :label="item.label" :value="item.value" />
+  </el-select>
+</template>
+<script>
+export default {
+  name: 'CrudSelect',
+  props: {
+    value: {
+      default() {
+        return this.multiple ? [] : ''
+      }
+    },
+    // multiple: {
+    //   type: Function,
+    //   default: false
+    // },
+    multiple: {
+      type: Boolean,
+      default: false
+    },
+    clearable: {
+      type: Boolean,
+      default: true
+    },
+    filterable: {
+      type: Boolean,
+      default: false
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    dic: {
+      default: () => {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      text: ''
+    }
+  },
+  watch: {
+    value: function(n, o) {
+      this.text = this.value
+    }
+  },
+  created() {
+    this.text = this.value
+  },
+  mounted() {
+  },
+  methods: {
+    handleChange(value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
